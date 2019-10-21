c8230539b080bc40c5d298e44f0a1a61b644db2f
Miles Smith <miles-smith@omrf.org>
2019-10-21 17:29:54 -0500

    Tested to works. Added test files and unit test.  Still figuring that last part out.
    Former-commit-id: c9949d9ee05a0ba73aa7384134c33870e703e0b5

--- s2a/convert.py
+++ s2a/convert.py
@@ -2,11 +2,12 @@ from anndata import AnnData
 from pandas import DataFrame
 import numpy as np
 from typing import List, Optional
+from collections import OrderedDict
 
 
-def add_meta_data(adata: AnnData, 
-                  md: DataFrame,
-                  column_for_rownames: Optional[str] = None) -> AnnData:
+def add_meta_data(
+    adata: AnnData, md: DataFrame, column_for_rownames: Optional[str] = None
+) -> AnnData:
     if "Unnamed: 0" in md.columns:
         md.index = md["Unnamed: 0"]
         md.drop(["Unnamed: 0"], axis=1)
@@ -17,28 +18,59 @@ def add_meta_data(adata: AnnData,
     return adata
 
 
-def add_feature_data(adata: AnnData,
-                     var_features: List[str],
-                     meta_features: DataFrame) -> AnnData:
-    
-    var_dict = {"n_cells": np.apply_along_axis(lambda x: (x>0).sum(), 1, adata.X),
-                "highly_variable": [True if _ in var_features else False for _ in adata.var.index],
-                "means": meta_features["means"],
-                "dispersions": meta_features['variance'],
-                "dispersions_norm": meta_features["variance.standardized"]}
-    adata.var = DataFrame.from_dict(data = var_dict)
+def add_feature_data(
+    adata: AnnData, var_features: List[str], meta_features: DataFrame
+) -> AnnData:
+
+    if "sct.mean" in meta_features.columns:
+        var_dict = {
+            "n_cells": np.apply_along_axis(lambda x: (x > 0).sum(), 0, adata.X),
+            "highly_variable": [
+                True if _ in var_features else False for _ in adata.var.index
+            ],
+            "means": meta_features["sct.mean"],
+            "dispersions": meta_features["sct.variance"],
+            "residuals_dispersions": meta_features["sct.residual_variance"],
+            "residual_mean": meta_features["sct.residual_mean"],
+            "detection_rate": meta_features["detection_rate"],
+        }
+    elif "vst.mean" in meta_features.columns:
+        var_dict = {
+            "n_cells": np.apply_along_axis(lambda x: (x > 0).sum(), 0, adata.X),
+            "highly_variable": [
+                True if _ in var_features else False for _ in adata.var.index
+            ],
+            "means": meta_features["vst.mean"],
+            "dispersions": meta_features["vst.variance"],
+            "dispersions_norm": meta_features["vst.variance.standardized"],
+        }
+    else:
+        var_dict = {
+            "n_cells": np.apply_along_axis(lambda x: (x > 0).sum(), 0, adata.X),
+            "highly_variable": [
+                True if _ in var_features else False for _ in adata.var.index
+            ],
+            "means": meta_features["mean"],
+            "dispersions": meta_features["variance"],
+            "dispersions_norm": meta_features["variance.standardized"],
+        }
+    adata.var = DataFrame.from_dict(data=var_dict)
     return adata
 
 
-def add_reduction(adata: AnnData,
-                   reduction_key: str,
-                   cell_embeddings: np.ndarray,
-                   feature_loadings: np.ndarray,
-                   reduction_sd: np.ndarray) -> AnnData:
-    
+def add_reduction(
+    adata: AnnData,
+    reduction_key: str,
+    cell_embeddings: np.ndarray,
+    feature_loadings: np.ndarray,
+    reduction_sd: np.ndarray,
+) -> AnnData:
+
     adata.obsm[f"X_{reduction_key}"] = cell_embeddings
     if feature_loadings.size > 0:
         adata.varm["PCs"] = feature_loadings
-    if reduction_sd.size > 0:
-        adata.uns[reduction_key]["variance"] = reduction_sd
+    if len(reduction_sd) > 0:
+        reduc = OrderedDict()
+        reduc["variance"] = reduction_sd
+        adata.uns[reduction_key] = reduc
     return adata
