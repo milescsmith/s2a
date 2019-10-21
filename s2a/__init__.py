f6e7b29a2cf87538903866c1f452663bd02abed1
Miles Smith <miles-smith@omrf.org>
2019-10-21 17:29:54 -0500

    Tested to works. Added test files and unit test.  Still figuring that last part out.

--- s2a/__init__.py
+++ s2a/__init__.py
@@ -3,11 +3,7 @@
 """Top-level package for s2a."""
 
 __author__ = """Miles Smith"""
-__email__ = 'miles-smith@omrf.org'
-__version__ = '0.1.0'
+__email__ = "miles-smith@omrf.org"
+__version__ = "0.1.0"
 
-from s2a.convert import (
-    add_meta_data,
-    add_feature_data, 
-    add_reduction
-)
+from s2a.convert import add_meta_data, add_feature_data, add_reduction
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(s2a)
+
+test_check("s2a")
