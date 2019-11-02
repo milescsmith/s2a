45aecb9d443bed62d9ed4878be6a880d52000b9a
Miles Smith <mileschristiansmith@gmail.com>
2019-11-02 18:40:20 -0500

    god im fucking sick of how reticulate can't find shit.
    Former-commit-id: db0671de239b7571f528c7001a0ea905551105bd

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-s2a <- NULL
-anndata <- NULL
-
 .onLoad <- function(libname, pkgname) {
-  s2a <<- reticulate::import(module = "s2a", delay_load = TRUE)
-  anndata <<- reticulate::import(module = "anndata", delay_load = TRUE)
   `%nin%` <- purrr::compose(`!`, `%in%`)
 }
