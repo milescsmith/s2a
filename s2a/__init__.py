9d1785daf3e851f03c4f34c79eaf3217a6f3ff7e
Miles Smith <mileschristiansmith@gmail.com>
2020-01-02 19:47:28 +0000

    Version bump

--- s2a/__init__.py
+++ s2a/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Miles Smith"""
 __email__ = "miles-smith@omrf.org"
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 from s2a.convert import add_meta_data, add_feature_data, add_reduction
