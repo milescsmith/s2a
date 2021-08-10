925b1d8c0e7461693a57b6f645bad2e89d4fdd56
Miles Smith <miles-smith@omrf.org>
2021-08-10 17:30:27 -0500

    Switch to Poetry

--- s2a/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for s2a."""
-
-__author__ = """Miles Smith"""
-__email__ = "miles-smith@omrf.org"
-__version__ = "0.1.2"
-
-from s2a.convert import add_meta_data, add_feature_data, add_reduction
---
+++ src/s2a/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from importlib.metadata import version
+"""Top-level package for s2a."""
+
+__author__ = """Miles Smith"""
+__email__ = "miles-smith@omrf.org"
+__version__ = version()
+
+from s2a.convert import add_feature_data, add_meta_data, add_reduction
