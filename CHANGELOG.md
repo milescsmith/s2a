93854ed926cac40f54cb1caff421fd525538299b
Miles Smith <mileschristiansmith@gmail.com>
2021-08-11 12:58:13 -0500

    fix: assay@meta.feature columns

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
+# [0.3.0]
+## Fixed
+  - Now check to see if columns in object[[assay]]@meta.features exist before
+    attempting to copy them
+  - call to `importlib_metadata.version()`
 
 # [0.2.0]
-
-## Added:
-- Setup pyre
-## Changed:
-- Switch to using Poetry
+## Added
+  - Setup pyre
+## Changed
+  - Switch to using Poetry
--- src/s2a/__init__.py
+++ src/s2a/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from importlib.metadata import version
+
 """Top-level package for s2a."""
 
 __author__ = """Miles Smith"""
 __email__ = "miles-smith@omrf.org"
-__version__ = version()
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 from s2a.convert import add_feature_data, add_meta_data, add_reduction
